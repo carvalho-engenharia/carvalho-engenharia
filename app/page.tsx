39ce4b7c422f6c20c9d539ac6e6b27c4895f7ae9
carvalhoengenharia2027-collab <carvalhoengenharia2027@gmail.com>
2026-05-16 13:20:46 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,6 @@ import { Services } from "@/components/services"
 import { PainPoints } from "@/components/pain-points"
 import { WhyUs } from "@/components/why-us"
 import { Sobre } from "@/components/sobre"
-import { Testimonials } from "@/components/terminal"
 import { CTASection } from "@/components/cta-section"
 import GoogleReviews from "@/components/google-reviews"
 import { Footer } from "@/components/footer"
@@ -18,7 +17,6 @@ export default function Home() {
       <PainPoints />
       <WhyUs />
       <Sobre />
-      <Testimonials />
       <CTASection />
       <GoogleReviews />
       <Footer />
